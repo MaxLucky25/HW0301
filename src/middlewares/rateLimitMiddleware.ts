97dcb7668fa362de4e0b30b003bd25fa9f52afbd
MaksLucky25 <maksrad93@gmail.com>
2025-04-16 18:00:18 +0300

    final changes 2

--- src/middlewares/rateLimitMiddleware.ts
+++ src/middlewares/rateLimitMiddleware.ts
@@ -1,14 +1,13 @@
 import { Request, Response, NextFunction } from 'express';
 import {requestLogsCollection} from "../db/mongo-db";
+import config from "../utility/config";
 
-const LIMIT_WINDOW_SECONDS = 10;
-const MAX_REQUESTS = 5;
 
 export const rateLimitMiddleware = async (req: Request, res: Response, next: NextFunction) => {
     const ip = req.ip;
     const url = req.originalUrl;
 
-    const windowStart = new Date(Date.now() - LIMIT_WINDOW_SECONDS * 1000);
+    const windowStart = new Date(Date.now() - config.LIMIT_WINDOW_SECONDS * 1000);
 
     const requestsCount = await requestLogsCollection.countDocuments({
         ip,
@@ -16,7 +15,7 @@ export const rateLimitMiddleware = async (req: Request, res: Response, next: Nex
         date: { $gte: windowStart },
     });
 
-    if (requestsCount > MAX_REQUESTS) {
+    if (requestsCount > config.MAX_REQUESTS) {
         res.status(429).send('Too many requests');
         return;
     }
--- src/utility/config.ts
+++ src/utility/config.ts
@@ -7,6 +7,8 @@ const config = {
     JWT_REFRESH_EXPIRES_IN: process.env.JWT_REFRESH_EXPIRES_IN || "20s",
     EMAIL_USER: process.env.EMAIL_USER || "your-email@gmail.com",
     EMAIL_PASS: process.env.EMAIL_PASS || "your-password",
+    LIMIT_WINDOW_SECONDS: Number(process.env.LIMIT_WINDOW_SECONDS) || 10,
+    MAX_REQUESTS: Number(process.env.MAX_REQUESTS) || 5,
 };
 
 export default config;
